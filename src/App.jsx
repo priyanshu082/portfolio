aba0bf40ab91f83a6be32efb172b54eb5b1aa6fa
priyanshu082 <priyanshusingh216@gmail.com>
2023-07-17 01:50:33 +0530

    navbar is started and all comaponents are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import {About ,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from "./components"
 
 function App() {
  
 
   return (
-   
-      <div className='bg-pink-500'>
-        priyanshu singh
-       </div>
-   
+  <BrowserRouter>
+  <div className='relative z-0 bg-primary'>
+   <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+    <Navbar/>
+    <Hero/>
+   </div>
+   <About/>
+   <Experience/>
+   <Tech/>
+   <Works/>
+   <Feedbacks/>
+  <div className='relative z-0'>
+    <Contact/>
+    <StarsCanvas/>
+  </div>
+  </div>
+  </BrowserRouter> 
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React,{useEffect,useState} from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from '../styles'
+import { navLinks } from '../constants'
+import {logo,menu,close} from "../assets"
+
+const Navbar = () => {
+  return (
+   <nav
+   className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <div className='w-full flex justify-between items-end max-w-7xl mx-auto'>
+
+    </div>
+
+   </nav>
+  )
+}
+
+export default Navbar;

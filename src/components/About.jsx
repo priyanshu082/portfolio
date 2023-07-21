cbed62c6986b40d16db91744325412c15c53e34d
priyanshu082 <priyanshusingh216@gmail.com>
2023-07-21 08:49:53 +0530

    hero portion is completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import {motion} from "framer-motion"
+
+import { styles } from '../styles'
+import { services } from '../constants'
+import {fadeIn,textVariant} from '../utils/motion'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>
+      Introduction
+      </p>
+      <h2 className={styles.sectionHeadText}>
+       Overview.
+      </h2>
+    </motion.div>
+    <motion.p
+    variants={fadeIn("","",0.1,1)}
+    className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+    HEY! My self <b className='text-[#915eff]'>PRIYANSHU SINGH</b>  currently pursuing my B.TECH from <b className='text-[#915eff]'>ZHCET,AMU(2026)</b>. 
+     I'm Skilled Webdeveloper with experience in <b className='text-[#915eff]'>Javascript</b> and also know about the frameworks like <b className='text-[#915eff]'>React</b>,<b className='text-[#915eff]'>Tailwind CSS</b>, working with <b className='text-[#915eff]'>APIs</b> ,<b className='text-[#915eff]'>Redux</b>  ,<b className='text-[#915eff]'>React Auth</b> and <b className='text-[#915eff]'>three.js</b>. I'm quick learner and can dedicate hours to work.<br/> 
+     Right now focusing on<b className='text-[#915eff]'> DSA</b>  along with Web development.
+    </motion.p>
+    </>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from 'react-three-fiber'
 import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
 
   const computer=useGLTF('./desktop_pc/scene.gltf')
 
@@ -22,8 +22,8 @@ const Computers = () => {
     castShadow
     shadow-mapsize={1024}/>
     <primitive 
-    scale={0.75}
-    position={[0,-3.25,-1.5]}
+    scale={isMobile ? 0.5:0.75}
+    position={isMobile ?[0,-2.5,-0.9]:[0,-3.25,-1.3]}
     rotation={[-0.01,-0.2,-0.1]}
     object={computer.scene}/>
    </mesh>
@@ -31,6 +31,31 @@ const Computers = () => {
 }
 
 const ComputersCanvas=()=>{
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(()=>{
+
+    const mediaQuery=window.matchMedia('(max-width:500px)');
+
+
+    // set the initial value"ismobile" state variable
+    setIsMobile(mediaQuery.matches);
+
+
+    //define a call back function to handle changes to the media query
+    const handleMediaQuerychanges=(event)=>{
+      setIsMobile(event.matches)
+    }
+
+    //add the call back funtion as a listener for changes to the media query
+    mediaQuery.addEventListener('change',handleMediaQuerychanges);
+ 
+    //remove the listener when the componenet is umounted
+    return()=>{
+      mediaQuery.removeEventListener('change',handleMediaQuerychanges);
+    }
+  },[])
+  
   return(
     <Canvas
     frameloop='demand'
@@ -43,7 +68,7 @@ const ComputersCanvas=()=>{
         enableZoom={false}
         maxPolarAngle={Math.PI/2}
         min={Math.PI/2}/>
-   <Computers/>
+   <Computers isMobile={isMobile}/>
       </Suspense>
  
 <Preload all/>

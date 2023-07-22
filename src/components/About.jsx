b80093a94831953ba83cbdd0fbf972f752663792
priyanshu082 <priyanshusingh216@gmail.com>
2023-07-22 11:02:04 +0530

    experience section is done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,33 +1,33 @@
 import React from 'react'
 import { Tilt } from 'react-tilt'
-import {motion} from "framer-motion"
-import {SectionWrapper} from '../hoc/SectionWrapper'
+import { motion } from "framer-motion"
+import { SectionWrapper } from '../hoc/SectionWrapper'
 
 import { styles } from '../styles'
 import { services } from '../constants'
-import {fadeIn,textVariant} from '../utils/motion'
+import { fadeIn, textVariant } from '../utils/motion'
 
 export const About = () => {
 
-  const ServiceCard=({index,title,icon})=>{
-    return(
-     <Tilt className="xs:w-[250px] w-full">
-         <motion.div
-         variants={fadeIn("right","spring",0.5*index,0.75)}
-         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card '>
+  const ServiceCard = ({ index, title, icon }) => {
+    return (
+      <Tilt className="xs:w-[250px] w-full">
+        <motion.div
+          variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+          className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card '>
           <div
-          options={{
-            max:45,
-            scale:1,
-            speed:450
-          }}
-          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+            options={{
+              max: 45,
+              scale: 1,
+              speed: 450
+            }}
+            className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
             <img src={icon} alt={title}
-            className='w-15 h-16 object-contain'/>
+              className='w-15 h-16 object-contain' />
             <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
           </div>
-         </motion.div>
-     </Tilt>
+        </motion.div>
+      </Tilt>
     )
 
   }
@@ -35,29 +35,29 @@ export const About = () => {
 
   return (
     <>
-    <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText}>
-      Introduction
-      </p>
-      <h2 className={styles.sectionHeadText}>
-       Overview.
-      </h2>
-    </motion.div>
-    <motion.p
-    variants={fadeIn("","",0.1,1)}
-    className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-    HEY! My self <b className='text-[#915eff]'>PRIYANSHU SINGH</b>  currently pursuing my B.TECH from <b className='text-[#915eff]'>ZHCET,AMU(2026)</b>. 
-     I'm Skilled Webdeveloper with experience in <b className='text-[#915eff]'>Javascript</b> and also know about the frameworks like <b className='text-[#915eff]'>React</b>,<b className='text-[#915eff]'>Tailwind CSS</b>, working with <b className='text-[#915eff]'>APIs</b> ,<b className='text-[#915eff]'>Redux</b>  ,<b className='text-[#915eff]'>React Auth</b> and <b className='text-[#915eff]'>three.js</b>. I'm quick learner and can dedicate hours to work.<br/> 
-     Right now focusing on<b className='text-[#915eff]'> DSA</b>  along with Web development.
-    </motion.p>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          Introduction
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Overview.
+        </h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+        HEY! My self <b className='text-[#915eff]'>PRIYANSHU SINGH</b>  currently pursuing my B.TECH from <b className='text-[#915eff]'>ZHCET,AMU(2026)</b>.
+        I'm Skilled Webdeveloper with experience in <b className='text-[#915eff]'>Javascript</b> and also know about the frameworks like <b className='text-[#915eff]'>React</b>,<b className='text-[#915eff]'>Tailwind CSS</b>, working with <b className='text-[#915eff]'>APIs</b> ,<b className='text-[#915eff]'>Redux</b>  ,<b className='text-[#915eff]'>React Auth</b> and <b className='text-[#915eff]'>three.js</b>. I'm quick learner and can dedicate hours to work.<br />
+        Right now focusing on<b className='text-[#915eff]'> DSA</b>  along with Web development.
+      </motion.p>
 
-   <div className='mt-20 flex flex-wrap flex-1 gap-10'>
-    {services.map((service,index)=>(
-      <ServiceCard key={service.title} index={index} {...service}/>
-    ))}
-    </div> 
+      <div className='mt-20 flex flex-wrap flex-1 gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   )
 }
 
-export default SectionWrapper(About,"about")
+export default SectionWrapper(About, "about")
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
+import { VerticalTimeline,VerticalTimelineElement } from 'react-vertical-timeline-component'
+import { motion } from 'framer-motion'
+import "react-vertical-timeline-component/style.min.css"
+import { styles } from '../styles'
+import { experiences } from '../constants'
+import {SectionWrapper} from '../hoc/SectionWrapper'
+import { textVariant } from '../utils/motion'
 
-const Experience = () => {
+export const Experience = () => {
+ 
+  const ExperienceCard=({experience})=>{
+    return(
+      <VerticalTimelineElement
+      contentStyle={{background:'#1d1836' ,color:'#fff'}}
+      contentArrowStyle={{borderRight:"7-px solid #232631"}}
+      iconStyle={{background:experience.iconBg}}
+      icon={
+        <div className='w-full h-full'>
+          <img 
+          src={experience.icon }
+          alt={experience.company_name}
+          className='object-contain'/>
+        </div>
+      }>
+       <div>
+        <h3 className='text-white text-[22px] font-bold'>{experience.title}</h3>
+        <p className='text-secondary text-[14px] font-semibold'
+        style={{margin:0}}>
+          {experience.company_name}
+        </p>
+       
+     
+       <ul className='mt-5 list-disc ml-5 space-y-2'>
+      {experience.points.map((point ,index)=>(
+        <li
+         key={`experience-point-${index}`}
+        className='text-white-100 text-[14px] pl-1 tracking-wider'>
+    {point}
+        </li>
+      ))}
+       </ul>
+       <div className='mt-[20px] ml-[10px] text-secondary text-[14px]'>{experience.date}</div>
+       </div>
+      </VerticalTimelineElement>
+    )
+  }
+
+
   return (
-    <div>Experience</div>
+    <>
+    <motion.div variants={textVariant()}>
+    <p className={styles.sectionSubText}>
+          What I have done so far
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Work Experience.
+        </h2>
+    </motion.div>
+
+    {/* verticla timeline  */}
+    <div className='mt-20 flex flex-col'>
+      <VerticalTimeline>
+
+     {experiences.map((experience,index)=>(
+      <ExperienceCard key={index} experience={experience}/>
+     ))}
+      </VerticalTimeline>
+    </div>
+    </>
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience,"work")

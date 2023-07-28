18ce4d9b20690344afc25a605af0c2a980241c1a
priyanshu082 <priyanshusingh216@gmail.com>
2023-07-28 23:12:54 +0530

    contact form started

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { motion } from "framer-motion"
+import emailjs from "@emailjs/browser"
+import { useRef,useState } from "react"
+import { styles } from "../styles"
+import { EarthCanvas } from "./canvas"
+import SectionWrapper from "../hoc/SectionWrapper"
+import { slideIn } from "../utils/motion"
 
 const Contact = () => {
+
+  const [form, setForm] = useState({name:'',email:'',message:''})
+
+  const [loading, setLoading] = useState(false)
+
+
   return (
     <div>Contact</div>
   )
